a7c37968c94343e67ddcaed1f98a632dab8535c0
cristianserrano25 <cristianserrano79@gmail.com>
2024-06-05 20:10:08 -0300

    se creo el repositorio

---
+++ README.md
@@ -0,0 +1,1 @@
+# TP_fetch_SerranoCristian
---
+++ script.js
@@ -0,0 +1,30 @@
+const link = "https://rickandmortyapi.com/api/character";
+
+const procesarfetch = async () => {
+    try {
+        const respuesta = await fetch(link);
+        const info = await respuesta.json();
+        const personajes = info.results.slice(0, 20);
+
+        const elgaleria = document.getElementById("galeria");
+        personajes.forEach((personaje) => {
+            // Crear el contenido HTML para cada personaje
+            const personajeHTML = `              
+                <div class="innerCont">
+                    <img src="${personaje.image}">
+                    <p>Name: ${personaje.name}</p>
+                    <p>Species: ${personaje.species}</p>
+                    <p>Status: ${personaje.status}</p>
+                </div>
+            `;
+            // Agregar el contenido HTML del personaje a la galería
+            elgaleria.innerHTML += personajeHTML;
+        });
+    } catch (error) {
+        console.log("Se produjo un error");
+    }
+}
+
+procesarfetch();
+
+
